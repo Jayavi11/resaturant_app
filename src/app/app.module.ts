a06928a12084430e14124849b5b9eeec897bebd2
Jayavi11 <jayavinayakjayakumar@gmail.com>
2022-07-08 07:27:55 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { PaymentComponent } from './payment/payment.component';
+import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    PaymentComponent,
+    SearchRestaurantComponent,
+    ViewfoodComponent
   ],
   imports: [
     BrowserModule,
